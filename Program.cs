c33c25ec3c94f284e8b531d39eb13901792e9188
79681 <79681@EVGENII>
2022-09-29 11:39:02 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Laba5_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string previousInput = "0";
+            int countOfOperations = 1000000;
+            for (int i = 0; i < countOfOperations; i++)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "") continue;
+                else if (input == "q") break;
+
+                if (int.TryParse(input, out int result)) Console.WriteLine((char)result);
+                if (Convert.ToDouble(input) == Convert.ToDouble(previousInput)) break;
+
+                previousInput = input;
+                countOfOperations++;
+            }
+        }
+    }
+}
